13e78794115718217e953fbc81871ce83f4e6c23
trimai_ <69742343+walshiiro@users.noreply.github.com>
2020-10-11 22:34:48 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+
+    bool a[10000];
+    memset(a,true,sizeof(a));
+    a[1]=false;
+    int i=2;
+    while (i<=trunc(sqrt(x)))
+    {
+        while(!a[i])
+        i++;
+        for(int j=i;i<=x/i;i++)
+            a[i*j]=false;
+        i++;
+
+
+
+
+    }
+    for(int i=1;i<=x;i++)
+        if(a[i])
+            cout<<i<<" ";
+
+
+}
